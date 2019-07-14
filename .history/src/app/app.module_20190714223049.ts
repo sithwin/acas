05d86dc5da296c20a7cb08a966a3dac35aff8f21
Si Thu Win <sthuwin@gmail.com>
2019-07-14 23:44:58 +0800

    import BrowserAnimationsModule

--- .history/src/app/app.module_20190714223049.ts
+++ .history/src/app/app.module_20190714223049.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
